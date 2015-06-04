20788cbbaf3e8b18a90a033af8bf9c013f63c918
polovka <myroslava.polovka@gmail.com>
2015-06-04 17:08:31 +0300

    prospectList and prospectCell have been added

--- CarusselSalesTool/MVC/Controllers/ProspectsControllers/ProspectsListViewController/ProspectsListViewController.m
+++ CarusselSalesTool/MVC/Controllers/ProspectsControllers/ProspectsListViewController/ProspectsListViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ProspectsListViewController.h"
+#import "CSTProspectCell.h"
+#import "UIView+MakeFromXib.h"
 
-@interface ProspectsListViewController ()
+@interface ProspectsListViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *prospectsTableView;
+@property (strong, nonatomic) NSArray *prospectsList;
+@property (weak, nonatomic) IBOutlet UISearchBar *prospectSearchBar;
 
 @end
 
@@ -19,6 +25,32 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.prospectsList = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4",@"5", nil];
+
+}
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.prospectsList count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CSTProspectCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([CSTProspectCell class])];
+    
+    if(!cell) {
+        cell = [CSTProspectCell makeFromXib];
+    }
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate 
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
