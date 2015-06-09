318aab7bae79c369b76db8e7afff093c9e7f4318
Eugenity <Eugenity@MacBook-Pro-MacBook.local>
2015-06-09 16:49:31 +0300

    search bar has been customized

--- CarusselSalesTool/MVC/Controllers/ProspectsControllers/ProspectsListViewController/ProspectsListViewController.m
+++ CarusselSalesTool/MVC/Controllers/ProspectsControllers/ProspectsListViewController/ProspectsListViewController.m
@@ -9,11 +9,12 @@
 #import "ProspectsListViewController.h"
 #import "CSTProspectCell.h"
 #import "UIView+MakeFromXib.h"
+#import "CSTCommonSearchBar.h"
 
 @interface ProspectsListViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *prospectsTableView;
-@property (weak, nonatomic) IBOutlet UISearchBar *prospectsSearchBar;
+@property (weak, nonatomic) IBOutlet CSTCommonSearchBar *prospectsSearchBar;
 
 @property (strong, nonatomic) NSArray *prospectsList;
 
@@ -27,8 +28,14 @@
 {
     [super viewDidLoad];
     self.prospectsList = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4",@"5", nil];
-
 }
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self customizeSearchBar];
+}
+
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -52,6 +59,14 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - Actions
+
+- (void)customizeSearchBar
+{
+    [self.prospectsSearchBar setRightImageName:@"icn_search"];
+    self.prospectsSearchBar.placeholder = NSLocalizedString(@"Name of prospect", nil);
 }
 
 @end
---
+++ CarusselSalesTool/MVC/Views/CommonSearchBar/CSTCommonSearchBar.m
@@ -0,0 +1,64 @@
+//
+//  CSTCommonSearchBar.m
+//  CarusselSalesTool
+//
+//  Created by Eugenity on 09.06.15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+#import "CSTCommonSearchBar.h"
+
+@implementation CSTCommonSearchBar
+
+#pragma mark - Accessors
+
+- (void)setRightImageName:(NSString *)rightImageName
+{
+    _rightImageName = rightImageName;
+    [self addRightSearchImage];
+}
+
+#pragma mark - Init
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+#pragma mark - Actions
+
+- (void)addRightSearchImage
+{
+    UITextField *searchField = [self valueForKey:@"_searchField"];
+    UIImageView *searchIcon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:self.rightImageName]];
+    
+    [searchField setRightView:searchIcon];
+    [searchField setLeftViewMode:UITextFieldViewModeNever];
+    [searchField setRightViewMode:UITextFieldViewModeAlways];
+    [searchField setClearButtonMode: UITextFieldViewModeNever];
+}
+
+- (void)commonInit
+{
+    [self setSearchFieldBackgroundImage:[UIImage imageNamed:@"search_field"] forState:UIControlStateNormal];
+    [self setSearchTextPositionAdjustment:UIOffsetMake(10.f, 0)];
+    [self setBarTintColor:[UIColor clearColor]];
+    
+    self.barTintColor = [UIColor clearColor];
+    self.backgroundImage = [UIImage new];
+}
+
+@end
