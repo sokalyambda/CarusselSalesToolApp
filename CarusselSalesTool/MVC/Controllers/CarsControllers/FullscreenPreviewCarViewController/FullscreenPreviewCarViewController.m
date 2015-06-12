d9eb46f09bb5ac7788ff3cb1087978d5217ec193
Anatoliy Dalekorey <admin@Mac-Admin.local>
2015-06-12 12:15:52 +0300

    added full screen preview

--- CarusselSalesTool/MVC/Controllers/CarsControllers/FullscreenPreviewCarViewController/FullscreenPreviewCarViewController.m
+++ CarusselSalesTool/MVC/Controllers/CarsControllers/FullscreenPreviewCarViewController/FullscreenPreviewCarViewController.m
@@ -10,13 +10,18 @@
 
 #import "CarImageCell.h"
 
-@interface FullscreenPreviewCarViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+typedef NS_ENUM(NSInteger, SelectedItem) {
+    SelectedItemPrev,
+    SelectedItemNext
+};
+
+@interface FullscreenPreviewCarViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
 @property (strong, nonatomic) CSTCar *car;
 
 @property (weak, nonatomic) IBOutlet UILabel *carNameLabel;
-@property (weak, nonatomic) IBOutlet UIImageView *fullScreenImageView;
-@property (weak, nonatomic) IBOutlet UICollectionView *carImagesCollectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *carThumbnailsCollectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *carOriginImageCollectionView;
 
 @end
 
@@ -39,51 +44,49 @@
     [self registerCell];
     
     self.carNameLabel.text = self.car.title;
-    [self selectCellWithIndex:0];
 }
 
 #pragma mark - Metods
 
-- (void)selectCellWithIndex:(NSInteger)index
+- (void)selectCellWithIndex:(NSInteger)index forCollectionView:(UICollectionView *)collectionView
 {
     NSIndexPath *selectIndex = [NSIndexPath indexPathForRow:index inSection:0];
-    [self.carImagesCollectionView selectItemAtIndexPath:selectIndex animated:YES scrollPosition:UICollectionViewScrollPositionCenteredHorizontally];
-}
-
-- (void)setFullScreenImageWithIndex:(NSInteger)index
-{
-    CSTImageCar *image = _car.images[index];
-    NSURL *imageUrl = [NSURL URLWithString:image.origUrl];
-    [self.fullScreenImageView sd_setImageWithURL:imageUrl completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-        //TO DO
-    }];
+    [collectionView selectItemAtIndexPath:selectIndex animated:YES scrollPosition:UICollectionViewScrollPositionCenteredHorizontally];
+    [self collectionView:collectionView didSelectItemAtIndexPath:selectIndex];
 }
 
 - (void)registerCell
 {
     NSString *nibName = NSStringFromClass([CarImageCell class]);
     UINib *cellNib = [UINib nibWithNibName:nibName bundle:nil];
-    [self.carImagesCollectionView registerNib:cellNib forCellWithReuseIdentifier:nibName];
+    [self.carThumbnailsCollectionView registerNib:cellNib forCellWithReuseIdentifier:nibName];
+    [self.carOriginImageCollectionView registerNib:cellNib forCellWithReuseIdentifier:nibName];
+}
+
+- (void)prevNextImage:(SelectedItem)item
+{
+    NSArray *selectedItems = [self.carOriginImageCollectionView indexPathsForVisibleItems];
+    NSIndexPath *indexPath = selectedItems[0];
+    NSInteger index = indexPath.row;
+    if (index > 0 && item == SelectedItemPrev) {
+        index--;
+    } else if (index < self.car.images.count - 1 && item == SelectedItemNext) {
+        index++;
+    }
+    [self selectCellWithIndex:index forCollectionView:self.carOriginImageCollectionView];
+    [self selectCellWithIndex:index forCollectionView:self.carThumbnailsCollectionView];
 }
 
 #pragma mark - Action
 
 - (IBAction)prevImage:(id)sender
 {
-    NSArray *selectedItems = [self.carImagesCollectionView indexPathsForSelectedItems];
-    NSIndexPath *indexPath = selectedItems[0];
-    if (indexPath.row > 0) {
-        [self selectCellWithIndex:indexPath.row - 1];
-    }
+    [self prevNextImage:SelectedItemPrev];
 }
 
 - (IBAction)nextImage:(id)sender
 {
-    NSArray *selectedItems = [self.carImagesCollectionView indexPathsForSelectedItems];
-    NSIndexPath *indexPath = selectedItems[0];
-    if (indexPath.row < self.car.images.count - 1) {
-        [self selectCellWithIndex:indexPath.row + 1];
-    }
+    [self prevNextImage:SelectedItemNext];
 }
 
 - (IBAction)back:(id)sender
@@ -101,11 +104,11 @@
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     CSTImageCar *image = self.car.images[indexPath.row];
-    NSURL *imageUrl = [NSURL URLWithString:image.thumbnailUrl];
+    NSURL *thumbnailUrl = [NSURL URLWithString:image.thumbnailUrl];
+    NSURL *originImagelUrl = [NSURL URLWithString:image.origUrl];
     
     CarImageCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([CarImageCell class]) forIndexPath:indexPath];
-    [cell.carImage sd_setImageWithURL:imageUrl];
-    
+    [cell.carImage sd_setImageWithURL:(collectionView == self.carThumbnailsCollectionView ? thumbnailUrl : originImagelUrl)];
     return cell;
 }
 
@@ -113,8 +116,23 @@
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    [self setFullScreenImageWithIndex:indexPath.row];
+    if (collectionView == self.carThumbnailsCollectionView) {
+        [self selectCellWithIndex:indexPath.row forCollectionView: self.carOriginImageCollectionView];
+    }
 }
 
+#pragma mark - UICollectionViewDelegateFlowLayout
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGSize size = CGSizeZero;
+    NSInteger height = collectionView.bounds.size.height - 10;
+    NSInteger width = collectionView.bounds.size.width;
+    if (collectionView == self.carThumbnailsCollectionView) {
+        width = height;
+    }
+    size = CGSizeMake(width, height);
+    return size;
+}
 
 @end
